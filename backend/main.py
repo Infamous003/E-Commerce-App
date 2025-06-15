f58fb9e728c9ac1b49eebd9a4d4b66d9aa287a0d
Infamous003 <arfathsyed0312@gmail.com>
2025-06-15 14:36:55 +0530

    Add routes to fetch all products and product by id

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
+from .routes import products
 
 app = FastAPI(
     title="E-Commerce backend",
     summary="This is a backend for an E-Commerce Application. Built using FastAPI.")
+
+app.include_router(products.router)
 
 @app.get("/")
 def root():
