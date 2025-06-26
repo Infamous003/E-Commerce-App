39ea07f4e9101858a12cdd54ab16501ea724c7a2
Infamous003 <arfathsyed0312@gmail.com>
2025-06-26 17:20:25 +0530

    Add endpoints to get cart items of the logged in user

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from .routes import auth, products, orders
+from .routes import auth, products, orders, cart
 from .database import init_db
 
 app = FastAPI(
@@ -9,6 +9,7 @@ app = FastAPI(
 app.include_router(products.router)
 app.include_router(auth.router)
 app.include_router(orders.router)
+app.include_router(cart.router)
 
 try:
     init_db()
--- backend/routes/cart.py
+++ backend/routes/cart.py
@@ -1,7 +1,16 @@
 from fastapi import APIRouter, HTTPException, status, Depends
-from ..models import Order, User, OrderStatus
+from ..models import Order, User, OrderStatus, Cart
 from ..database import get_session
 from sqlmodel import Session, select
 from .auth import get_current_user
 
 router = APIRouter(prefix="/cart", tags=["Shopping cart"])
+
+@router.get("/")
+def get_my_cart_items(session: Session = Depends(get_session),
+                      current_user: User = Depends(get_current_user)):
+    cart_items = session.exec(
+        select(Cart).where(Cart.customer_id == current_user.id)
+    ).fetchall()
+
+    return cart_items
