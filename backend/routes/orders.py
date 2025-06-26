6573d39edefff0a6f8531c012f6a76d9127f6d04
Infamous003 <arfathsyed0312@gmail.com>
2025-06-26 12:55:00 +0530

    Modify get_orders to return the logged in user's orders and add a new endpoint for getting details of a specific order with consistant http messages for all the routes

--- backend/routes/orders.py
+++ backend/routes/orders.py
@@ -7,12 +7,27 @@ from .auth import get_current_user
 router = APIRouter(prefix="/orders", tags=["Orders"])
 
 @router.get("/")
-def get_orders(session: Session = Depends(get_session)):
+def get_my_orders(session: Session = Depends(get_session),
+                  current_user: User = Depends(get_current_user)):
     orders = session.exec(
-        select(Order)
+        select(Order).where(Order.customer_id == current_user.id)
     ).fetchall()
 
+    if not orders:
+        return {"data": "You do not have any orders"}
     return orders
+
+@router.get("/{id}")
+def get_order_details(id: int,
+                      session: Session = Depends(get_session),
+                      current_user: User = Depends(get_current_user)):
+    order = session.exec(
+        select(Order).where(Order.id == id, Order.customer_id == current_user.id)
+    ).one_or_none()
+
+    if order is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Order not found")
+    return order
 
 @router.get("/{id}/cancel")
 def cancel_order(id: int,
@@ -24,7 +39,7 @@ def cancel_order(id: int,
     ).one_or_none()
 
     if order_found is None or order_found.status == OrderStatus.CANCELED:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"You do not have an order with ID:{id}")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"You do not have an active order with ID:{id}")
 
     order_found.status = OrderStatus.CANCELED
     session.add(order_found)
@@ -42,7 +57,7 @@ def return_order(id: int,
     ).one_or_none()
 
     if order_found is None or order_found.status == OrderStatus.RETURNED or order_found.status == OrderStatus.CANCELED:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Order not found")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"You do not have an active order with ID:{id}")
     
     order_found.status = "RETURNED"
     session.add(order_found)
